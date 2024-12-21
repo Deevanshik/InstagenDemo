63ac3411225576c1fc8efc8037a9272e2dd333fe
Deevanshik <137674395+Deevanshik@users.noreply.github.com>
2024-12-21 10:45:14 +0530

    Changed

--- instagen_scripts/demo_instagen.sh
+++ instagen_scripts/demo_instagen.sh
@@ -2,7 +2,7 @@ cd mmdetection/
 
 python demo.py \
     --detector_config configs/instagen/instagen-4scale_fd_8xb2-12e_coco_demo.py \
-    --detector_ckpt checkpoints/instagen-4scale_fd_8xb2-12e_coco.pth \
+    --detector_ckpt /kaggle/input/instagen-model-weights/pytorch/default/1/instagen-4scale_fd_8xb2-12e_coco.pth \
     --cls_names 'motorcycle, umbrella' \
     --score_thr 0.4 \
     --out demo/instagen_pred.jpg
